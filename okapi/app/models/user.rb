5882e06143941739ee3900a428af75823b812eef
EC2 Default User <ec2-user@ip-172-31-0-209.us-east-2.compute.internal>
2019-02-20 05:15:37 +0000

    added full functionality to user account creation

--- okapi/app/models/user.rb
+++ okapi/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-     validates(:name, presence: true)
+     validates(:first_name, presence: true)
      validates :email, presence: true
      validates :password, presence: true
 end
