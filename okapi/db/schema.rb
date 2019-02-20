9ab0f90024816d3ef60108f40b149662e0e5739a
EC2 Default User <ec2-user@ip-172-31-0-209.us-east-2.compute.internal>
2019-02-20 01:49:51 +0000

    Updated Schema for Table

--- okapi/db/schema.rb
+++ okapi/db/schema.rb
@@ -14,11 +14,12 @@
 ActiveRecord::Schema.define(version: 20190219210117) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
