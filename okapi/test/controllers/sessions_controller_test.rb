81a8356ddde253b7ae13e4c64ff18797ab1c9e1a
Robert Osonma <35873421+master248@users.noreply.github.com>
2019-02-19 21:13:18 -0600

    added login and create user backend (paired with Brianna Witte)

--- okapi/test/controllers/sessions_controller_test.rb
+++ okapi/test/controllers/sessions_controller_test.rb
@@ -2,8 +2,19 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
   test "should get new" do
-    get :new
+    #get :new
+    get login_path
     assert_response :success
+  end
+
+  test "login is invalid" do
+  get login_path
+  assert_template 'session/new'
+  post login_path, params: {session: {email: "", password: ""}}
+  assert_template 'sessions/new'
+  assert_not flash.empty?
+  get root_path
+  assert flash.empty?
   end
 
 end
