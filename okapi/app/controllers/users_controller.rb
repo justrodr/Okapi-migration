d34b9eee8460b5c6b8215e75dbb78db9c093c63d
EC2 Default User <ec2-user@ip-172-31-38-129.us-east-2.compute.internal>
2019-02-21 17:15:50 +0000

    users controller updated

--- okapi/app/controllers/users_controller.rb
+++ okapi/app/controllers/users_controller.rb
@@ -3,10 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
-  def show
-    @user = User.find(params[:id])
+  #def show
+  #  @user = User.find(params[:id])
     #debugger
-  end
+ # end
   
   def create
     @user = User.new(user_params)
