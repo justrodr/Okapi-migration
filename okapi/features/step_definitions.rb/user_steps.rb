686a8d0b010d4a3d1ebe699056794a38665f0fe9
witte_breezy <witte_breezy@tamu.edu>
2019-02-24 22:43:05 -0600

    started framework for cucumber testing

---
+++ okapi/features/step_definitions.rb/user_steps.rb
@@ -0,0 +1,12 @@
+
+Given("I am on the home page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click Create Account") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I want to be taken to the create account page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
---
+++ okapi/features/user_login.feature
@@ -0,0 +1,11 @@
+Feature: User login/Create new user
+	As a property owner
+	So that I can create and access my account
+	I want to manage my properties and information
+
+
+    Scenario: Creating a new account
+        Given I am on the home page
+        When I click Create Account 
+        Then I want to be taken to the create account page
+        
