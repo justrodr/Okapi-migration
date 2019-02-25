4b63f64401e1c382c22d203cc34d2f7eb3522f76
witte_breezy <witte_breezy@tamu.edu>
2019-02-25 14:24:55 -0600

    added more scenarios

--- okapi/features/step_definitions/user_steps.rb
+++ okapi/features/step_definitions/user_steps.rb
@@ -2,10 +2,38 @@
     visit root_path
   end
   
-  When("I click Create Account") do
-    pending # Write code here that turns the phrase above into concrete actions
+  When("I click on Create Account") do
+    click_on("Create Account")
   end
   
   Then("I want to be taken to the create account page") do
-    pending # Write code here that turns the phrase above into concrete actions
+    assert page.current_path, 'users/new'
   end
+  
+  When("I click on login") do
+    click_on("Login")
+  end
+  
+  Then("I want to be taken to the login page") do
+    assert page.current_path, '/login'
+  end
+
+
+
+
+
+  Given("I am on the login page") do
+    visit "/login"
+  end
+  
+  When("I enter invalid login information") do
+    /fill_in 'email', with: 'badexample@gmail.com'
+    fill_in('password', with: 'failure')/
+  end
+  
+  Then("I should see an error message") do
+    /expect(page).to have_content("Invalid email or password")/
+  end
+
+  
+  
--- okapi/features/user_login.feature
+++ okapi/features/user_login.feature
@@ -3,9 +3,20 @@ Feature: User login/Create new user
 	So that I can create and access my account
 	I want to manage my properties and information
 
-
     Scenario: Creating a new account
         Given I am on the home page
-        When I click Create Account 
+        When I click on Create Account 
         Then I want to be taken to the create account page
-        
+    
+    Scenario: Login to account
+        Given I am on the home page
+        When I click on login
+        Then I want to be taken to the login page
+
+    Scenario: Invalid Login information
+        Given I am on the login page
+        When I enter invalid login information
+        Then I should see an error message
+
+    
+
