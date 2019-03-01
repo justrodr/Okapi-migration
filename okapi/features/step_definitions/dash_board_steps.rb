b7b62e587eabde2d66859779a1e680a28f246e9f
witte_breezy <witte_breezy@tamu.edu>
2019-02-28 20:30:29 -0600

    added scenarios and steps for dashboard

---
+++ okapi/features/step_definitions/dash_board_steps.rb
@@ -0,0 +1,47 @@
+Given("I am on the Dashboard page") do
+    visit "/dash"
+  end
+  
+  Then("I should see all of my properties") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click on Contacts on the sidebar") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should be on my Contacts page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click on Payment on the sidebar") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should be on my Payment page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click on Orders on the sidebar") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should be on my Orders page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click on Profile on the sidebar") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I should be on my Profile page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When("I click on Add Property") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("a window should pop up to add a property") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
