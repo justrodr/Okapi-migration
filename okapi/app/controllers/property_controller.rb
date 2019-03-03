99ede2caad12221dc199002f91558aa85a6c6006
Robert Osonma <35873421+master248@users.noreply.github.com>
2019-03-03 16:21:54 -0600

    made edits to property_controller

--- okapi/app/controllers/property_controller.rb
+++ okapi/app/controllers/property_controller.rb
@@ -1,4 +1,15 @@
 class PropertyController < ApplicationController
   def new
+    @property = Property.new
   end
+
+  def create
+    @property = Property.new(prop_name)
+    @current_user = @current_user || User.find_by(id: session[:user_id])
+    @current_user.prop_index = @property.id #validate that user name matches property ID
+  end
+  private 
+      def prop_params
+        params.require(:property).permit(:prop_name, :tenant_name, :tenant_email, :tenant_phone, :address, :frequency)
+      end
 end
