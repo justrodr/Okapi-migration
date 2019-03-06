9d1f87089af423bf82fb3d4a18bda40340b0ef5d
EC2 Default User <ec2-user@ip-172-31-38-129.us-east-2.compute.internal>
2019-03-06 00:10:23 +0000

    added link functionality to splash screen buttons

---
+++ okapi/app/controllers/contact_controller.rb
@@ -0,0 +1,11 @@
+class ContactController < ApplicationController
+    def new
+    end
+    
+    def show
+       # redirect_to :controller => 'users', :action => 'new'
+    end
+
+    def create
+    end
+end
