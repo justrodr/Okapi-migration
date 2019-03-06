0a27e365afc33db0969a22803ff3d1c2bd7c2884
Arianne Malagayo <ArianneMalagayo@Ariannes-MacBook-Pro.local>
2019-03-05 19:26:13 -0600

    Pulled from master

--- okapi/config/routes.rb
+++ okapi/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'property/new'
+
   root 'splash_screen#new'
   get 'users/new' => 'users#new'
-  post 'users/new' => 'users#create'
+  post 'users' => 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   get    '/dash',   to: 'dash_board#new'
