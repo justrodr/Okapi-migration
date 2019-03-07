082bdb40ddd22c2df2f6bbaabf8d7ee7e89d4e85
Justin Rodriguez <justinrodriguez@tamu.edu>
2019-03-07 17:23:33 +0000

    table loads properties from database

--- okapi/app/controllers/dash_board_controller.rb
+++ okapi/app/controllers/dash_board_controller.rb
@@ -2,5 +2,8 @@ class DashBoardController < ApplicationController
     def new
         @user = User.find_by(session[:user_id])
         puts @user.id, "****************"
+        @property = Property.all
+        puts @user.id, "****************"
     end
+
 end
