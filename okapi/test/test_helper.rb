82f041cad10593f5068d25fa8a43c3ab068d4aec
witte_breezy <witte_breezy@tamu.edu>
2019-03-07 18:30:25 -0600

    fully implemented SimpleCov

--- okapi/test/test_helper.rb
+++ okapi/test/test_helper.rb
@@ -1,13 +1,12 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
 
-# spec/spec_helper.rb
-require 'simplecov'
-require 'simplecov-console'
-SimpleCov.formatter = SimpleCov::Formatter::Console
-SimpleCov.start
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
