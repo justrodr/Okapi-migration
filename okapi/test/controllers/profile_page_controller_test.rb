cce18873bbfc18febf064d29203c593f939262b8
Hunter Hewitt <hunterhewitt@tamu.edu>
2019-03-18 13:41:45 -0500

    Revert "Hunterhewitt branch1"

--- okapi/test/controllers/profile_page_controller_test.rb
+++ okapi/test/controllers/profile_page_controller_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class ProfilePageControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
