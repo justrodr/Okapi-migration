d82147c0aff47432febf8dbe199920990a17ab32
Robert Osonma <35873421+master248@users.noreply.github.com>
2019-03-24 10:59:02 -0500

    Fixed issue where error messages would print twice

--- okapi/app/models/user.rb
+++ okapi/app/models/user.rb
@@ -4,9 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
      before_save { email.downcase! }
-     validates(:first_name, presence: true)
-     validates(:last_name, presence: true)
-     validates :email, presence: true, uniqueness: { case_sensitive: false }
-     validates :password, presence: true, length: { minimum: 6 }
+     #validates(:first_name, presence: true)
+     #validates(:last_name, presence: true)
+     #validates :email, presence: true, uniqueness: { case_sensitive: false }
+     #validates :password, presence: true, length: { minimum: 6 }
      #has_secure_password
 end
