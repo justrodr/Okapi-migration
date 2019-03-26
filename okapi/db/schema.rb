0cbe1aef2db7821cd2768f01401539d7e264ae12
Arianne Malagayo <ArianneMalagayo@Ariannes-MacBook-Pro.local>
2019-03-26 11:35:59 -0500

    Implemented Devise from Robert

--- okapi/db/schema.rb
+++ okapi/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190321141230) do
+ActiveRecord::Schema.define(version: 20190326163332) do
 
   create_table "properties", force: :cascade do |t|
     t.integer  "user"
