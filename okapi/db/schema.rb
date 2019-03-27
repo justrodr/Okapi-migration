6a1fdd74d503bae424ed8ab846acb34b60062d8a
witte_breezy <witte_breezy@tamu.edu>
2019-03-26 19:41:45 -0500

    fixed devise db stuff

--- okapi/db/schema.rb
+++ okapi/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190321141230) do
+ActiveRecord::Schema.define(version: 20190327003354) do
 
   create_table "properties", force: :cascade do |t|
     t.integer  "user"
