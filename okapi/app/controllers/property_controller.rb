eb4ddba738cecadd529a61ecd241d46b2f20382d
Arianne Malagayo <ArianneMalagayo@Ariannes-MacBook-Pro.local>
2019-03-26 20:57:31 -0500

    Added to property_controller.rb

--- okapi/app/controllers/property_controller.rb
+++ okapi/app/controllers/property_controller.rb
@@ -1,21 +1,27 @@
 class PropertyController < ApplicationController
   def new
     @property = Property.new
+    # respond_to do |format|
+    #   format.html
+    #   format.js
+    # end
   end
 
   def create
     @current_user =  User.find_by(email: session[:email])
     @property = Property.new(prop_params)
-    if(@current_user)
-      @property.user = @current_user.id
-    end
+    @property.user = @current_user.id
     if(@property.save)
+      puts "Property Saved"
+      @properties = Property.where(user: User.find_by(email: session[:email]))
       redirect_to dash_path
     else
       puts @property.errors.messages
+      puts "Property Not Saved"
       redirect_to contact_path ###FIXME
     end
   end
+
   private 
       def prop_params
         params.require(:property).permit(:prop_name, :tenant_name, :tenant_email, :tenant_phone, :address, :frequency)
