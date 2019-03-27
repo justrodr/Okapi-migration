0e19b37eb31200ecb2f3fe5ce1ee08ff54b49db0
Arianne Malagayo <ArianneMalagayo@Ariannes-MacBook-Pro.local>
2019-03-26 20:56:41 -0500

    Added @properties at dash_board_controller

--- okapi/app/controllers/dash_board_controller.rb
+++ okapi/app/controllers/dash_board_controller.rb
@@ -5,6 +5,7 @@ class DashBoardController < ApplicationController
             @user = User.find_by(email: session[:email]) #why does first id make this nil in cucumber test
             puts session[:email]
         end
-        @property = Property.all
+        @property = Property.new
+        @properties = Property.all
     end
 end
