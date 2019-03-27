18130277786f4c55599471241ef5e7706dd141be
Robert Osonma <35873421+master248@users.noreply.github.com>
2019-03-26 19:50:58 -0500

    fixed database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190325030121) do
+ActiveRecord::Schema.define(version: 20190327004902) do
 
   create_table "properties", force: :cascade do |t|
     t.integer  "user"
