6702aafb3fe1aee4fdb78f995fe6d4dd4618d10e
witte_breezy <witte_breezy@tamu.edu>
2019-04-04 11:37:10 -0500

    pull from develop

--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,9 @@ ActiveRecord::Schema.define(version: 20190403005440) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.datetime "last_delivered"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zipcode"
   end
 
   create_table "users", force: :cascade do |t|
