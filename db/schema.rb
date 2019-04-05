43016e9e91965c0b1a92ea9aeb7bf8bd00470139
Justin Rodriguez <justinrodriguez@tamu.edu>
2019-04-05 00:04:57 +0000

    fixed merge overwrites

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190403005440) do
+ActiveRecord::Schema.define(version: 20190403005613) do
 
   create_table "orders", force: :cascade do |t|
     t.string   "shipping_address"
