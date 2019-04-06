b83a8055df42ddfca84a05a5d07e93707d5f5df0
EC2 Default User <ec2-user@ip-172-31-38-129.us-east-2.compute.internal>
2019-04-06 03:14:44 +0000

    hopefully got order page done with minimal bugs

--- app/controllers/dash_board_controller.rb
+++ app/controllers/dash_board_controller.rb
@@ -1,19 +1,5 @@
 class DashBoardController < ApplicationController
     def splash 
-        session[:size_strings] = {:size10b20 => "10\" x 20\" x 1\"", :size14b20 => "14\" x 20\" x 1\"", :size16b24  => "16\" x 24\" x 1\"",
-                :size18b30 => "18\" x 30\" x 1\"", :size12b12 => "12\" x 12\" x 1\"", :size14b24 => "14\" x 24\" x 1\"", :size16b25 => "16\" x 25\" x 1\"", 
-                :size20b20 => "20\" x 20\" x 1\"", :size12b20 => "12\" x 20\" x 1\"", :size14b25 => "14\" x 25\" x 1\"", :size18b18 => "18\" x 18\" x 1\"", 
-                :size20b24 => "20\" x 24\" x 1\"", :size12b24 => "12\" x 24\" x 1\"", :size14b30 => "14\" x 30\" x 1\"", :size18b20 => "18\" x 20\" x 1\"",
-                :size20b25 => "20\" x 25\" x 1\"", :size12b30 => "12\" x 30\" x 1\"", :size15b20 => "15\" x 30\" x 1\"", :size18b24 => "18\" x 24\" x 1\"", 
-                :size20b30 => "20\" x 30\" x 1\"", :size12b36 => "12\" x 36\" x 1\"", :size16b20 => "16\" x 20\" x 1\"", :size18b25 => "18\" x 25\" x 1\"", 
-                :size24b24 => "24\" x 24\" x 1\"", :size25b25 => "25\" x 25\" x 1\""}
-        session[:price] = {:size10b20 => 30, :size14b20 => 24, :size16b24  => 40,
-                :size18b30 => 48, :size12b12 => 24, :size14b24 => 38, :size16b25 => 41, 
-                :size20b20 => 40, :size12b20 => 32, :size14b25 => 39, :size18b18 => 36, 
-                :size20b24 => 44, :size12b24 => 36, :size14b30 => 34, :size18b20 => 38,
-                :size20b25 => 45, :size12b30 => 42, :size15b20 => 35, :size18b24 => 42, 
-                :size20b30 => 50, :size12b36 => 48, :size16b20 => 36, :size18b25 => 43, 
-                :size24b24 => 48, :size25b25 => 50}
         if(!(session[:email].nil?))
             redirect_to dash_path
         end
--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,4 +1,5 @@
 class OrderController < ApplicationController
+
     def new
         @order = Order.new
     end
@@ -7,7 +8,38 @@ class OrderController < ApplicationController
         #@order = Order.new(order_params)
     end
     
+    def view
+    end
+    
+    def paypal
+        @order = session[:order]
+        @order.save
+        puts "***************************"
+        puts @order.id
+        puts "***************************"
+        redirect_to orders_page_path
+    end
+    
+    def orders_page
+        @all_orders = Order.where(user: User.find_by(email: session[:email]).id)
+    end
+
     def checkout
+       session[:order].destroy
+       session[:sizes] = {"size10b20"=>"10\" x 20\" x 1\"","size14b20"=>"14\" x 20\" x 1\"","size16b24"=>"16\" x 24\" x 1\"",
+               "size18b30"=>"18\" x 30\" x 1\"","size12b12"=>"12\" x 12\" x 1\"","size14b24"=>"14\" x 24\" x 1\"","size16b25"=>"16\" x 25\" x 1\"", 
+               "size20b20"=>"20\" x 20\" x 1\"","size12b20"=>"12\" x 20\" x 1\"","size14b25"=>"14\" x 25\" x 1\"","size18b18"=>"18\" x 18\" x 1\"", 
+               "size20b24"=>"20\" x 24\" x 1\"","size12b24"=>"12\" x 24\" x 1\"","size14b30"=>"14\" x 30\" x 1\"","size18b20"=>"18\" x 20\" x 1\"",
+               "size20b25"=>"20\" x 25\" x 1\"","size12b30"=>"12\" x 30\" x 1\"","size15b20"=>"15\" x 20\" x 1\"","size18b24"=>"18\" x 24\" x 1\"", 
+               "size20b30"=>"20\" x 30\" x 1\"","size12b36"=>"12\" x 36\" x 1\"","size16b20"=>"16\" x 20\" x 1\"","size18b25" =>"18\" x 25\" x 1\"", 
+               "size24b24"=>"24\" x 24\" x 1\"","size25b25"=>"25\" x 25\" x 1\""}
+        session[:price_hash] = {"size10b20"=> 30,"size14b20"=> 34,"size16b24"=> 40,
+               "size18b30"=> 48,"size12b12"=> 24,"size14b24"=> 38,"size16b25"=> 41, 
+               "size20b20"=> 40,"size12b20"=> 32,"size14b25"=> 39,"size18b18"=> 36, 
+               "size20b24"=> 44,"size12b24"=> 36,"size14b30"=> 34,"size18b20"=> 38,
+               "size20b25"=> 45,"size12b30"=> 42,"size15b20"=> 35,"size18b24"=> 42, 
+               "size20b30"=> 50,"size12b36"=> 48,"size16b20"=> 36,"size18b25"=> 43, 
+               "size24b24"=> 48,"size25b25"=> 50}
         @current_user = User.find_by(email: session[:email])
         @order = Order.new(order_params)
         @order.property = session[:property]
@@ -21,17 +53,29 @@ class OrderController < ApplicationController
         @order.zipcode = @property.zipcode
         @order.frequency = @property.frequency
         @order.user = @current_user
-        if @order.save
-            #redirect_to checkout_path(@order)
-        else
-            redirect_to order_path(@property)
-            flash[:warning] = "property not saved"
+        small_keys = []
+        total_price = [] 
+        keys = @order.attributes.keys
+        keys.each do |subs| 
+         if subs.include? "size" 
+             small_keys.push subs 
+         end
+        end 
+        small_keys.each do |key|
+            if(@order.attributes[key]) 
+                total_price.push session[:price_hash][key]*@order.attributes[key]
+            end 
         end
+        @order.price = total_price.inject(0){|sum,x| sum + x }+7.00 #plus 7 is for shipping
+        puts @order.price
+        puts @property.errors.messages
+        session[:order] = @order
+        redirect_to view_checkout_path(@order)
     end
     
     private
         def order_params
-            params.require(:order).permit(:shipping_address, :tenant_name, :tenant_email, :property, :start_date,:order_status, :size10b20, :size14b20, :size16b24,
+            params.require(:order).permit(:shipping_address, :price, :tenant_name, :tenant_email, :property, :start_date,:order_status, :size10b20, :size14b20, :size16b24,
                 :size18b30, :size12b12, :size14b24, :size16b25, :size20b20, :size12b20, :size14b25, :size18b18, :size20b24, :size12b24, :size14b30, :size18b20,
                 :size20b25, :size12b30, :size15b20, :size18b24, :size20b30, :size12b36, :size16b20, :size18b25, :size24b24, :size25b25, :user)
         end
