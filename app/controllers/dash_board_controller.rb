40bba73cff6f872e9b1fe753059adf4f93848c0e
Robert Osonma <35873421+master248@users.noreply.github.com>
2019-04-07 22:39:11 -0500

    improved admin login

--- app/controllers/dash_board_controller.rb
+++ app/controllers/dash_board_controller.rb
@@ -28,16 +28,16 @@ class DashBoardController < ApplicationController
     
     # def orders
     # end
+
+    def admin
+        @all_orders = Order.all
+    end
     
     def new
         session[:log] = 1
         if(!session.nil?)
             @user = User.find_by(email: session[:email]) #why does first id make this nil in cucumber test
             puts session[:email]
-        end
-        if (@user.admin)
-            #@all_orders = Order.all
-            redirect_to admin_path
         end
         @property = Property.new
         session[:properties] = Property.where(id: User.find_by(email: session[:email]))
